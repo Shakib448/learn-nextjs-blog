32a4041990824af1c1310c8f4e4b9fd727832ae7
Shakib448 <shakiba448@gmail.com>
2021-02-19 07:02:23 +0600

    Routing

--- pages/index.js
+++ pages/index.js
@@ -1,98 +1,20 @@
 import Head from "next/head";
-import Link from "next/link";
+import Layout, { siteTitle } from "../components/Layout/layout";
+import utilStyles from "../styles/utils.module.scss";
 
 export default function Home() {
   return (
-    <div className="container">
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
-
-      <main>
-        <h1 className="title">
-          Read{" "}
-          <Link href="/posts/first-post">
-            <a>this page!</a>
-          </Link>
-        </h1>
-      </main>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title {
-          text-align: center;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-          line-height: 1.6;
-          font-size: 18px;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-
-        a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-        a:hover {
-          text-decoration: underline;
-        }
-
-        img {
-          max-width: 100%;
-          display: block;
-        }
-      `}</style>
-    </div>
+      <section className={utilStyles.headingMd}>
+        <p>Hi this is Muktadir. I'm software engineer</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
   );
 }
